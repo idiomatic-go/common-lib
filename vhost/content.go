b26e3329b61f47318c8be76cfd9dd9ad65d2836b
idiomatic-go <markbwilson@outlook.com>
2022-11-30 06:53:44 -0600

    Update content.go

--- vhost/content.go
+++ vhost/content.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/idiomatic-go/common-lib/eventing"
 	"github.com/idiomatic-go/common-lib/fse"
+	"github.com/idiomatic-go/common-lib/logxt"
 	"reflect"
 )
 
@@ -28,7 +29,9 @@ func AccessCredentials(msg *eventing.Message) Credentials {
 func ProcessContent[T any](content any) (T, Status) {
 	var t T
 	if IsNil(content) {
-		return t, NewStatusInvalidArgument(errors.New(fmt.Sprintf("vhost.ProcessContent internal error : no content available")))
+		status := NewStatusInvalidArgument(errors.New(fmt.Sprintf("vhost.ProcessContent internal error : no content available")))
+		logxt.LogDebugf("%v", status)
+		return t, status
 	}
 	if buf, ok := content.(fse.Entry); ok {
 		t1, err := fse.ProcessEntry[T](&buf)
@@ -50,17 +53,23 @@ func ProcessContent[T any](content any) (T, Status) {
 	if t1, ok := content.(T); ok {
 		return t1, NewStatusOk()
 	}
-	return t, NewStatusInvalidArgument(errors.New(fmt.Sprintf("vhost.ProcessContent internal error : invalid content type : %v", reflect.TypeOf(content))))
+	status := NewStatusInvalidArgument(errors.New(fmt.Sprintf("vhost.ProcessContent internal error : invalid content type : %v", reflect.TypeOf(content))))
+	logxt.LogDebugf("%v", status)
+	return t, status
 }
 
 func ProcessContextContent[T any](ctx context.Context) (T, Status) {
 	var t T
 	if ctx == nil {
-		return t, NewStatusInvalidArgument(errors.New(fmt.Sprintf("vhost.ProcessContent internal error : context is nil")))
+		status := NewStatusInvalidArgument(errors.New(fmt.Sprintf("vhost.ProcessContent internal error : context is nil")))
+		logxt.LogDebugf("%v", status)
+		return t, status
 	}
 	i := ctx.Value(contentKey)
 	if IsNil(i) {
-		return t, NewStatusInvalidArgument(errors.New(fmt.Sprintf("vhost.ProcessContent internal error : no content available")))
+		status := NewStatusInvalidArgument(errors.New(fmt.Sprintf("vhost.ProcessContent internal error : no content available")))
+		logxt.LogDebugf("%v", status)
+		return t, status
 	}
 	return ProcessContent[T](i)
 }
